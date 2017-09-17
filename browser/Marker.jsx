1c55e99af74e5366d28a3129d477d233490043a5
Beth Qiang <bethany.qiang@gmail.com>
2017-09-17 14:09:08 -0500

    Add single filters for year & category

--- browser/Marker.jsx
+++ browser/Marker.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Marker = props => (
-  <div style={{ height: 10, width: 10, backgroundColor: 'red', borderRadius: '50%' }} />
+  <div style={{ height: 10, width: 10, backgroundColor: props.markerColor, borderRadius: '50%' }} />
 );
 
 export default Marker;
